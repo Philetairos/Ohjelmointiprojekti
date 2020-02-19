250ef142eb2a11b434360cc045223ca65f057736
Philetairos <orkoniitty@hotmail.com>
2020-02-20 00:17:15 +0200

    Projektin aloitus

---
+++ Ohjelmointiprojekti/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RLNET;
+
+//Ohjelman päätiedosto, Daniel Juola 
+//Perustuu Faron Bracyn esimerkkikoodiin
+//Projektissa käytetyt kirjastot: RLNet (Travis M. Clark, 2014) ja RogueSharp (Faron Bracy, 2014-2019), MIT-lisenssi
+
+namespace Ohjelmointiprojekti
+{
+    class Program
+    {
+        //Pääkonsolin koko tiileinä, ei pikseleinä
+        private static readonly int konsolileveys = 160;
+        private static readonly int konsolikorkeus = 80;
+        //Muiden konsolien koko perustuu sitten pääkonsolin kokoon
+        private static readonly int karttaleveys = Convert.ToInt32(konsolileveys *0.75);
+        private static readonly int karttakorkeus = Convert.ToInt32(konsolikorkeus * 0.8);
+        private static readonly int sivukonsolileveys = Convert.ToInt32(konsolileveys * 0.25);
+        private static readonly int dialogikonsolikorkeus = Convert.ToInt32(konsolikorkeus * 0.2);
+        private static readonly int konsolikorkeuspuolet = Convert.ToInt32(konsolikorkeus * 0.5);
+
+        //Luo kaikki konsolit
+        private static RLRootConsole paaKonsoli;
+        private static RLConsole karttaKonsoli;
+        private static RLConsole dialogiKonsoli;
+        private static RLConsole inventaarioKonsoli;
+        private static RLConsole statsiKonsoli;
+
+        public static void Main()
+        {
+            //Fontti jota tiilit ja teksti käyttävät
+            string fonttiTiedosto = "terminal8x8.png";
+            string konsoliNimi = "Paragon of Virtue";
+            //Luo pääkonsoli joka toimii pelin perustana
+            paaKonsoli = new RLRootConsole(fonttiTiedosto, konsolileveys, konsolikorkeus, 8, 8, 1, konsoliNimi);
+            //Luo karttakonsoli joka toimii pelimaailman näkymänä
+            karttaKonsoli = new RLConsole(karttaleveys, karttakorkeus);
+            //Luo dialogikonsoli joka näyttää pelin viestit ja keskustelun dialogin
+            dialogiKonsoli = new RLConsole(karttaleveys, dialogikonsolikorkeus);
+            //Luo inventaatiokonsoli joka näyttää pelaajan esineet
+            inventaarioKonsoli = new RLConsole(sivukonsolileveys, konsolikorkeuspuolet);
+            //Luo statistiikkakonsoli joka näyttää pelaajan hahmo(je)n tilan
+            statsiKonsoli = new RLConsole(sivukonsolileveys, konsolikorkeuspuolet);
+            paaKonsoli.Update += PaivitaKonsoli;
+            paaKonsoli.Render += PiirraKonsoli;
+            paaKonsoli.Run();
+        }
+        
+        private static void PaivitaKonsoli(object sender, UpdateEventArgs e)
+        {
+            karttaKonsoli.SetBackColor(0, 0, karttaleveys, karttakorkeus, RLColor.Black);
+            karttaKonsoli.Print(0,0,"testiKartta", RLColor.White);
+            dialogiKonsoli.SetBackColor(0, 0, karttaleveys, dialogikonsolikorkeus, RLColor.Blue);
+            dialogiKonsoli.Print(0, 0, "testiDialogi", RLColor.White);
+            inventaarioKonsoli.SetBackColor(0, 0, sivukonsolileveys, konsolikorkeuspuolet, RLColor.Blue);
+            inventaarioKonsoli.Print(0, 0, "testiInventaario", RLColor.White);
+            statsiKonsoli.SetBackColor(0, 0, sivukonsolileveys, konsolikorkeuspuolet, RLColor.Blue);
+            statsiKonsoli.Print(0, 0, "testiStatsit", RLColor.White);
+        }
+        
+        private static void PiirraKonsoli(object sender, UpdateEventArgs e)
+        {
+            RLConsole.Blit(karttaKonsoli, 0, 0, karttaleveys, karttakorkeus, paaKonsoli, 0, 0);
+            RLConsole.Blit(dialogiKonsoli, 0, 0, karttaleveys, dialogikonsolikorkeus, paaKonsoli, 0, karttakorkeus);
+            RLConsole.Blit(inventaarioKonsoli, 0, 0, sivukonsolileveys, konsolikorkeuspuolet, paaKonsoli, karttaleveys, konsolikorkeuspuolet);
+            RLConsole.Blit(statsiKonsoli, 0, 0, sivukonsolileveys, konsolikorkeuspuolet, paaKonsoli, karttaleveys, 0);
+            paaKonsoli.Draw();
+        }
+    }
+}
